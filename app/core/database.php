a510a6c33d0dffdd7397c2942d99ad5536bf91a7
FEARricepudding <jordanrandles@googlemail.com>
2022-02-20 23:25:43 +0000

    Removed dotenv dep

--- app/core/database.php
+++ app/core/database.php
@@ -11,11 +11,11 @@ class db {
 	
 	public static function getInstance() {
 		if(!self::$objInstance){
-			$db_host = $_ENV["db_hostname"];
+			$db_host = $_ENV["db_host"];
 			$db_name = $_ENV["db_name"];	
 			$dsn = "mysql:dbname=$db_name;host=$db_host";
-			self::$objInstance = new PDO($dsn, $_ENV["db_username"], $_ENV["db_password"]);
-        	self::$objInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			self::$objInstance = new PDO($dsn, $_ENV["db_user"], $_ENV["db_pass"]);
+			self::$objInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
         return self::$objInstance;
 	}
