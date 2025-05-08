82549397ae3bbc1b1471c207a3dd531f2afb08a0
FEARricepudding <jordanrandles@googlemail.com>
2025-05-08 23:06:29 +0100

    Update how we call router

--- server.php
+++ server.php
@@ -9,11 +9,11 @@ use uranium\core\ConfigHandler;
 
 register_shutdown_function(array('uranium\core\ErrorHandler', 'check_for_fatal'));
 set_error_handler(array('uranium\core\ErrorHandler', 'log_error'));
-
 ini_set("display_errors", "off");
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 
 ScriptLoader::folder(__DIR__."/scripts");
 
 @session_start();
-router::init();
+$router = new Router;
+$router->init();
