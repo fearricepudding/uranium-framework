25a4c49308f552a40bd5aaa9114a661205c3551f
FEARricepudding <jordanrandles@googlemail.com>
2026-01-18 20:44:17 +0000

    Need a dummy sql method

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -32,18 +32,4 @@ final class ModelTest extends TestCase{
         $whereAndTest = $model->whereAnd(["something"=>"value", "else"=>"anotherValue"]);
         $this->assertSame($expected, $whereAndTest->getSelectors());
     }
-
-    public function testModelGet(): void{
-        $model1 = new UserModel();
-        $model1->test = true;
-        $sql1 = $model1->whereAnd(["username"=>"test", "email"=>"anotherValue"])->get();
-        $expected = "SELECT `id`, `username`, `active` FROM User WHERE `username`=:username  AND `email`=:email ";
-        $this->assertSame($expected, $sql1);
-
-        $model2 = new UserModel();
-        $model2->test = true;
-        $sql2 = $model2->withProtected()->get();
-        $expected = "SELECT `id`, `username`, `email`, `password`, `active` FROM User";
-        $this->assertSame($expected, $sql2);
-    }
 }
